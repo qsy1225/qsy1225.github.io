9cce470745d29dae2c13af6161c788f907620b05
qsy1225 <1903959708@qq.com>
2025-02-15 16:13:19 +0800

    Site updated: 2025-02-15 16:13:18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1739520387039";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1739607187294";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
